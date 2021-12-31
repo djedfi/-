3c58a0646062caab86af76f16f404f0ae798743d
Edwin Figueroa <fh040234@gmail.com>
2021-12-31 02:16:03 -0500

    first commit

---
+++ database/migrations/2021_12_31_064919_options_app.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OptionsApp extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options_app', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',45);
+            $table->string('description',255)->nullable();
+            $table->string('path_option',20)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('options_app');
+    }
+}
---
+++ database/migrations/2021_12_31_070132_user_options.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserOptions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('user_optios',function(Blueprint $table)
+        {
+            //relaciones
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                            ->references('id')
+                            ->on('users')
+                            ->onUpdate('cascade')
+                            ->onDelete('restrict');
+            $table->unsignedBigInteger('option_id');
+            $table->foreign('option_id')
+                            ->references('id')
+                            ->on('options_app')
+                            ->onUpdate('cascade')
+                            ->onDelete('restrict');     
+            $table->primary(['user_id', 'option_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
